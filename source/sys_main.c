7f89202946fbae27c29b1d155d9b23b1d517c672
Sohal Patel <tosohal@gmail.com>
2018-06-18 17:54:00 +0200

    First Commit

---
+++ source/sys_main.c
@@ -0,0 +1,154 @@
+/** @file sys_main.c 
+*   @brief Application main file
+*   @date 07-July-2017
+*   @version 04.07.00
+*
+*   This file contains an empty main function,
+*   which can be used for the application.
+*/
+
+/* 
+* Copyright (C) 2009-2016 Texas Instruments Incorporated - www.ti.com 
+* 
+* 
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions 
+*  are met:
+*
+*    Redistributions of source code must retain the above copyright 
+*    notice, this list of conditions and the following disclaimer.
+*
+*    Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the   
+*    distribution.
+*
+*    Neither the name of Texas Instruments Incorporated nor the names of
+*    its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+
+/* USER CODE BEGIN (0) */
+/* USER CODE END */
+
+/* Include Files */
+
+#include "sys_common.h"
+
+/* USER CODE BEGIN (1) */
+/* Include FreeRTOS scheduler files */
+#include "FreeRTOS.h"
+#include "os_task.h"
+
+/* Include HET header file - types, definitions and function declarations for system driver */
+#include "het.h"
+#include "gio.h"
+#include "sci.h"
+#include "spi.h"
+/* Define Task Handles */
+xTaskHandle xTask1Handle;
+xTaskHandle xTask2Handle;
+void    sciDisplayText      (sciBASE_t *sci, uint8_t *text,uint32_t length);
+
+/* Task1 */
+void vTask1(void *pvParameters)
+{
+    for(;;)
+    {
+        /* Taggle HET[1] with timer tick */
+        gioSetBit(hetPORT1, 17, gioGetBit(hetPORT1, 17) ^ 1);
+        vTaskDelay(100);
+    }
+}
+
+/* Task2 */
+void vTask2(void *pvParameters)
+{
+    spiDAT1_t SPI3DataFormat;
+    SPI3DataFormat.CSNR = SPI_CS_0;
+    SPI3DataFormat.CS_HOLD = TRUE;
+    SPI3DataFormat.DFSEL = SPI_FMT_0;
+    SPI3DataFormat.WDEL = FALSE;
+
+    for(;;)
+    {
+        /* Taggle HET[1] with timer tick */
+        gioSetBit(hetPORT1, 0, gioGetBit(hetPORT1, 0) ^ 1);
+        sciDisplayText(scilinREG, "sohal/r", 6);
+        spiTransmitData(spiREG3, &SPI3DataFormat, 2, (uint16_t*)"hi");
+        vTaskDelay(500);
+    }
+}
+
+/* USER CODE END */
+
+/** @fn void main(void)
+*   @brief Application main function
+*   @note This function is empty by default.
+*
+*   This function is called after startup.
+*   The user can use this function to implement the application.
+*/
+
+/* USER CODE BEGIN (2) */
+/* USER CODE END */
+
+uint8	emacAddress[6U] = 	{0xFFU, 0xFFU, 0xFFU, 0xFFU, 0xFFU, 0xFFU};
+uint32 	emacPhyAddress	=	0U;
+
+int main(void)
+{
+/* USER CODE BEGIN (3) */
+    /* Set high end timer GIO port hetPort pin direction to all output */
+    gioSetDirection(hetPORT1, 0xFFFFFFFF);
+    spiInit();
+    sciInit();
+
+    /* Create Task 1 */
+    if (xTaskCreate(vTask1,"Task1", configMINIMAL_STACK_SIZE, NULL, 1, &xTask1Handle) != pdTRUE)
+    {
+        /* Task could not be created */
+        while(1);
+    }
+
+    /* Create Task 2 */
+    if (xTaskCreate(vTask2,"Task2", configMINIMAL_STACK_SIZE, NULL, 2, &xTask2Handle) != pdTRUE)
+    {
+        /* Task could not be created */
+        while(1);
+    }
+    /* Start Scheduler */
+    vTaskStartScheduler();
+
+    /* Run forever */
+    while(1);
+/* USER CODE END */
+
+    return 0;
+}
+
+
+/* USER CODE BEGIN (4) */
+void sciDisplayText(sciBASE_t *sci, uint8_t *text,uint32_t length)
+{
+    while(length--)
+    {
+        while ((sci->FLR & 0x4) == 4); /* wait until busy */
+        sciSendByte(sci,*text++);      /* send out text   */
+    };
+}
+/* USER CODE END */
